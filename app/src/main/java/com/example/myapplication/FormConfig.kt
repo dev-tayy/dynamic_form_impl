62ec0ab6b34845c0ef0144b416a3b45bff7c4436
Tijani <atytjani@gmail.com>
2024-08-17 07:02:26 +0100

    chore: setup asset manager and load config file

---
+++ app/src/main/java/com/example/myapplication/FormConfig.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication
+
+import com.google.gson.annotations.SerializedName
+
+data class FormConfig(
+    @SerializedName("forms") val forms: Map<String, Form>
+)
+
+data class Form(
+    @SerializedName("pages") val pages: Map<String, Page>
+)
+
+data class Page(
+    @SerializedName("fields") val fields: Map<String, Field>
+)
+
+data class Field(
+    @SerializedName("ui_type") val uiType: String,
+    @SerializedName("column_type") val columnType: String,
+    @SerializedName("column_name") val columnName: String,
+    @SerializedName("required") val required: Boolean,
+    @SerializedName("min_length") val minLength: Int,
+    @SerializedName("max_length") val maxLength: Int,
+    @SerializedName("showOnList") val showOnList: Boolean? = null,
+    @SerializedName("values") val values: List<String>? = null
+)
