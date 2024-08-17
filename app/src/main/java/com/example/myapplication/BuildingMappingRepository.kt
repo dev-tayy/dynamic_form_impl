27e15b72734296e1454833a1d9570d9eb23dac65
Tijani <atytjani@gmail.com>
2024-08-17 07:37:34 +0100

    chore: write BuildingMappingRepository impl

---
+++ app/src/main/java/com/example/myapplication/BuildingMappingRepository.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+class BuildingMappingRepository(private val buildingMappingDao: BuildingMappingDao) {
+
+    /**
+     * Inserts a new building mapping into the database.
+     *
+     * @param buildingMapping The BuildingMapping object to insert.
+     * @return The ID of the newly inserted building mapping.
+     */
+    suspend fun insert(buildingMapping: BuildingMapping): Long {
+        return buildingMappingDao.insertBuildingMapping(buildingMapping)
+    }
+
+    /**
+     * Retrieves all building mappings from the database.
+     *
+     * @return A list of all BuildingMapping objects.
+     */
+    suspend fun getAll(): List<BuildingMapping> {
+        return buildingMappingDao.getAllMappings()
+    }
+}
